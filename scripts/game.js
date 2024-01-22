67fbe5fbf03da2125d1c7b3eaa401f1bc927f89a
SPHsofi <Sofia_pash@mail.ru>
2024-01-22 15:11:02 +0300

    feat: add section and div for game place

---
+++ scripts/game.js
@@ -0,0 +1,20 @@
+import { pic } from "./pic.js"; 
+
+// функция для создания секции с игрой
+export function createGameSection() {
+  const gameSection = document.createElement('section');
+  gameSection.className = 'game__section section';
+  
+  const gameFill = createGameFill();
+  gameSection.append(gameFill);
+
+  return gameSection; 
+}
+
+// функция для добавления игрового поля
+function createGameFill() {
+  const gameFill = document.createElement('div');
+  gameFill.className = 'game-wraper';
+
+  return gameFill; 
+}

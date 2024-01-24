7516c2a498bbf0e4ae2d3827d96fa0696ce3e38a
SPHsofi <Sofia_pash@mail.ru>
2024-01-24 16:55:00 +0300

    fead: insert other module into index.js and insert into main

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,6 @@
 import { createGameSection } from "./game.js";
+import { createSelectUl } from "./list.js";
+import { createCheckbox } from "./level.js";
 
 const main = document.createElement('main');
 main.className = 'main';
@@ -6,7 +8,13 @@ main.className = 'main';
 const headSection = document.createElement('section');
 headSection.className = 'head__section section';
 
+const levelDiv = createCheckbox();
 const gameSection = createGameSection();
+const optionSection = createSelectUl();
+// const optionalFell = createOptionFell();
+
+headSection.appendChild(levelDiv)
+headSection.appendChild(optionSection);
 
 main.appendChild(headSection);
 main.appendChild(gameSection);

d63633192749b5b81b452418c0ae0a7f30caa6d6
SPHsofi <Sofia_pash@mail.ru>
2024-01-26 15:17:45 +0300

    refactor: change binary array to array objects with true and false

---
+++ scripts/pics.js
@@ -0,0 +1,187 @@
+export const pics = [
+  {
+    'size': 5,
+    'name': 'Flower',
+    'pic':
+      [
+        [{ isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: false },
+        ],
+        [{ isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        ],
+        [{ isShouldClick: true },
+        { isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: true },
+        ],
+        [{ isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        ],
+        [{ isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: false },
+        ],
+      ]
+  },
+  {
+    'size': 5,
+    'name': 'Dinosaur',
+    'pic':
+      [
+        [{ isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: true }
+        ],
+        [{ isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: false }
+        ],
+        [{ isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: false }
+        ],
+        [{ isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: false }
+        ],
+        [{ isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: false }
+        ]
+      ]
+  },
+  {
+    'size': 5,
+    'name': 'Car',
+    'pic':
+      [
+        [{ isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: false }
+        ],
+        [{ isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: false }
+        ],
+        [{ isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true }
+        ],
+        [{ isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true }
+        ],
+        [{ isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: false }
+        ]
+      ]
+  },
+  {
+    'size': 5,
+    'name': 'Snake',
+    'pic':
+      [
+        [{ isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true }
+        ],
+        [{ isShouldClick: true },
+        { isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: false }
+        ],
+        [{ isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true }
+        ],
+        [{ isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: true }
+        ],
+        [{ isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true }
+        ]
+      ]
+  },
+  {
+    'size': 5,
+    'name': 'Dog',
+    'pic':
+      [
+        [{ isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: false }
+        ],
+        [{ isShouldClick: true },
+        { isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true }
+        ],
+        [{ isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: true },
+        { isShouldClick: false }
+        ],
+        [{ isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: false }
+        ],
+        [{ isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: false },
+        { isShouldClick: true },
+        { isShouldClick: false }
+        ]
+      ],
+  },
+]

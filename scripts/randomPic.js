a794438b6099ef29485ec80cef3bf26f4868ef13
SPHsofi <Sofia_pash@mail.ru>
2024-01-30 19:52:37 +0300

    feat: add button random picture

---
+++ scripts/randomPic.js
@@ -0,0 +1,23 @@
+import { pics } from "./pics.js";
+import { renderField, renderInfoGame } from "./index.js";
+
+export function renderRandomBtn() {
+  const randomBtnWrapper = document.createElement('div');
+  randomBtnWrapper.className = 'random-wrapper';
+  
+  const randomBtn = document.createElement('button');
+  randomBtn.className = 'random__btn';
+  randomBtn.textContent = 'Random picture';
+
+  randomBtn.onclick = function() {
+    const randomIndex = Math.floor(Math.random() * pics.length);
+    const randomGameFill = pics[randomIndex];
+    renderField(randomGameFill.pic, randomGameFill.size);
+    renderInfoGame(randomGameFill.name);
+  }
+
+  randomBtnWrapper.appendChild(randomBtn);
+
+  return randomBtnWrapper;
+}
+

b5c653a2471672eeeef4c1edfcdec69c2bb031f0
SPHsofi <Sofia_pash@mail.ru>
2024-02-01 10:31:57 +0300

    feat: add event switch theme

--- scripts/theme.js
+++ scripts/theme.js
@@ -9,6 +9,22 @@ export function changeTheme() {
   svgImg.src = 'images/moon.svg';
   svgImg.className = 'moon__svg';
 
+  changeThemeBtn.addEventListener('click', () => {
+    const stylesheet = document.getElementById('stylesheet');
+    if (stylesheet.href.includes('light')) {
+      stylesheet.href = 'styles/index-dark.css';
+      setTimeout(() => {
+        svgImg.src = 'images/sun.svg';
+      })
+    } else {
+      stylesheet.href = 'styles/index-light.css';
+      setTimeout(() => {
+        svgImg.src = 'images/moon.svg';
+      })
+    }
+  })
+
+
   changeThemeBtn.appendChild(svgImg);
 
   themeChangeWrapper.appendChild(changeThemeBtn);

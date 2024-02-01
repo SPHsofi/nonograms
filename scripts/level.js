1996293341aeee76dc169cb6be37db0926bf39af
SPHsofi <Sofia_pash@mail.ru>
2024-02-01 16:30:45 +0300

    refactor: add star svg in level btn

--- scripts/level.js
+++ scripts/level.js
@@ -17,6 +17,22 @@ function levelButtons(text) {
   const button = document.createElement('button');
   button.className = 'level__btn';
   button.textContent = text;
+  
+  const svgIcon = document.createElement('img');
+  svgIcon.className = 'star__btn';
+  svgIcon.src = 'images/light-star.svg';
+
+  if (text === 'Easy') {
+    button.appendChild(svgIcon);
+   } else if (text === 'Medium') {
+    for (let i = 0; i < 2; i++) {
+      button.appendChild(svgIcon.cloneNode(true));
+    }
+   } else {
+    for (let i = 0; i < 3; i++) {
+      button.appendChild(svgIcon.cloneNode(true));
+    }
+   }
 
   button.addEventListener('click', () => {
     button.className = 'select-level';

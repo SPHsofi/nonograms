2d28d7c2423a9c96e76286cf0023c7e60333c743
SPHsofi <Sofia_pash@mail.ru>
2024-02-02 14:34:51 +0300

    feat: add function render rating table

--- scripts/modalWin.js
+++ scripts/modalWin.js
@@ -16,13 +16,13 @@ export function createModalWin() {
 
   const levelText = document.createElement('p');
   levelText.className = 'modal-text';
-  levelText.textContent = 'Level passed';
-  
+  levelText.textContent = `Your time ${timer.counter} sec`;
+
   const buttonExit = document.createElement('button');
   buttonExit.className = 'modal-exit__btn'
   buttonExit.textContent = 'Exit';
 
-  buttonExit.addEventListener('click', () =>{
+  buttonExit.addEventListener('click', () => {
     const exitSound = document.querySelector('.exit__audio');
     exitSound.play();
 
@@ -35,11 +35,48 @@ export function createModalWin() {
 
   btnContainer.appendChild(buttonExit);
 
+  const rating = ratingTable();
+
   modalWrapper.appendChild(header);
   modalWrapper.appendChild(levelText);
+  modalWrapper.appendChild(rating);
   modalWrapper.appendChild(btnContainer);
 
   modalSection.appendChild(modalWrapper);
 
   return modalSection;
+}
+
+function ratingTable() {
+  const ratingWrapper = document.createElement('div');
+  ratingWrapper.className = 'rating-wrapper';
+
+  const ratingTableLs = JSON.parse(localStorage.getItem('winGame'));
+  const ratingTable = ratingTableLs.slice(0, 5);
+
+  const ratingText = document.createElement('p');
+  ratingText.className = 'modal-rating';
+  ratingText.textContent = 'Rating table';
+
+  ratingWrapper.appendChild(ratingText);
+  
+  ratingTable.forEach((el, ind) => {
+    const ratingIndex = document.createElement('p');
+    ratingIndex.className = 'rating-index';
+    ratingIndex.textContent = ind + 1;
+
+    const ratingName = document.createElement('p');
+    ratingName.className = 'rating-name';
+    ratingName.textContent = el.name;
+
+    const ratingTime = document.createElement('p');
+    ratingTime.className = 'rating-time';
+    ratingTime.textContent = `${el.time} sec`;
+
+    ratingWrapper.append(ratingIndex);
+    ratingWrapper.append(ratingName);
+    ratingWrapper.append(ratingTime);
+  });
+
+  return ratingWrapper;
 }
